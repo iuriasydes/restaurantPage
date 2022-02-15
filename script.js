2144787832e2cd3a8379b6659754b0014ab4e7d3
iuriasydes <iuriasydes@gmail.com>
2022-02-15 11:41:40 -0800

    Seccion De Blogs Terminada

--- script.js
+++ script.js
@@ -75,3 +75,25 @@ let swiper3 = new Swiper(".menu-slider", {
     clickable: true,
   },
 });
+/*Slide de Blogs*/
+let swiper4 = new Swiper(".blogs-slider", {
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
+});
